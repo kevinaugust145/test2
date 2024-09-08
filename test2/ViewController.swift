7086573d263aacaa81f5c8c9bdfb5883651df0a1
KevinHung <kevinaugust145@gmail.com>
2024-09-08 10:57:54 +0800

    Initial Commit

---
+++ test2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test2
+//
+//  Created by 洪宗燦 on 2024/9/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ test2Tests/test2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  test2Tests.swift
+//  test2Tests
+//
+//  Created by 洪宗燦 on 2024/9/6.
+//
+
+import Testing
+@testable import test2
+
+struct test2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
